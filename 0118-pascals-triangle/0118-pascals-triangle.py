34e77955be55e6d3dc72f65781d2699eaad1ac8d
phuongngu100 <135727156+phuongngu100@users.noreply.github.com>
2026-04-14 23:30:27 -0400

    Time: 0 ms (100%), Space: 19.2 MB (94.09%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,17 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        #DP easy
+        res = []
+        for n in range(numRows):
+            row = [None for _ in range(n+1)] # initiate an array with space for that round, then fill top and bottom with 1 cause they always begin and start with 1
+            row[0] = 1
+            row[-1] = 1
+
+            for j in range(1, len(row) -1):
+                row[j] = res[n-1][j-1] + res[n-1][j] # n is the way down chieu doc, j is length chieu dai of an array 
+            res.append(row)
+        return res
+
+
+
+        

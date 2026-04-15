c8ada48a2520d377cd2607ebd099be9a6baf169b
phuongngu100 <135727156+phuongngu100@users.noreply.github.com>
2026-04-14 23:48:02 -0400

    Time: 0 ms (100%), Space: 31.1 MB (94.14%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -1,11 +1,25 @@
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
         l, r = 0, len(arr) -1
-        while l <= r:
-            if arr[l] < arr[r]:
-                l += 1
-            else:
-                r -= 1
 
+        while l <r:
+            mid = (l+r) // 2
+            if arr[mid] < arr[mid+1]: # peak is greater than mid
+                l = mid+1
+            else: #peak is either mid or some index smaller than mid
+                r = mid
         return l
+
+
+
+
+
+        # time worst case O(n) cause still have to traverse the whole thing
+        # while l <= r:
+        #     if arr[l] < arr[r]:
+        #         l += 1
+        #     else:
+        #         r -= 1
+
+        # return l
         

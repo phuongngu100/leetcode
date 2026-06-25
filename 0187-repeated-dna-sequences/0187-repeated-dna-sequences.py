7e116946d713883a8b99a2c29d983fee7bc89f98
phuongngu100 <135727156+phuongngu100@users.noreply.github.com>
2026-06-25 07:26:04 -0400

    Time: 33 ms (29.01%), Space: 32.3 MB (13.38%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -4,14 +4,14 @@ class Solution:
         1. Using hashset. Time is O(n)
         '''
 
-        seen, res = set(), set()
+        # seen, res = set(), set()
 
-        for l in range(len(s)-9):
-            cur = s[l : l+10]
-            if cur in seen:
-                res.add(cur)
-            seen.add(cur)
-        return list(res)
+        # for l in range(len(s)-9):
+        #     cur = s[l : l+10]
+        #     if cur in seen:
+        #         res.add(cur)
+        #     seen.add(cur)
+        # return list(res)
 
         '''
         2. Using hashmap

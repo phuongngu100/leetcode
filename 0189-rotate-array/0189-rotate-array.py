20169e05917bf2f421d7bfaeb5660b1ddc02ddd5
phuongngu100 <135727156+phuongngu100@users.noreply.github.com>
2026-06-28 03:07:03 -0400

    Time: 14 ms (12.79%), Space: 26.4 MB (70.04%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,26 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k %= n
+
+        # start = starting position of the cycle
+        # count = how many elements we have moved
+        count = start = 0
+
+        while count < n: # the loop stop when we have moved all the elements around
+            cur, prev = start, nums[start]
+            while True:
+                next_idx = (cur + k) % n 
+                nums[next_idx], prev = prev, nums[next_idx] #switch
+                cur = next_idx
+                count +=1
+                if start == cur:
+                    break
+            start +=1
+            
+
+        
+        

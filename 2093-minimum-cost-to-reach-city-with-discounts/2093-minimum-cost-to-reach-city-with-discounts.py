36573981d2b3dfb9f85a9b0ff5191d7970a75a32
phuongngu100 <135727156+phuongngu100@users.noreply.github.com>
2026-08-12 14:26:54 +0700

    Time: 369 ms (83.33%), Space: 38.6 MB (65.08%) - LeetHub

---
+++ 2093-minimum-cost-to-reach-city-with-discounts/2093-minimum-cost-to-reach-city-with-discounts.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minimumCost(self, n: int, highways: List[List[int]], discounts: int) -> int:
+        heap = [(0, 0, 0)] # cost, start, discount used
+        graph = [[] for _ in range(n)]
+        for a, b, toll in highways: # since this is bidirectional
+            graph[a].append((b, toll))
+            graph[b].append((a, toll))
+        dist = [[float('inf')] * (discounts+1) for _ in range(n)]
+        dist[0][0] = 0
+
+        while heap:
+            cost, city, used = heapq.heappop(heap)
+            if city == n-1:
+                return cost
+            if cost > dist[city][used]:
+                continue
+            for nei, toll in graph[city]:
+                # option 1: dont use discount
+                new_cost = cost + toll
+                if new_cost < dist[nei][used]:
+                    dist[nei][used] = new_cost
+                    heapq.heappush(heap, (new_cost,nei,used))
+         
+                # option 2: use discount
+                if used < discounts:
+                    new_cost = cost + toll//2
+                    new_used = used + 1
+                    if new_cost < dist[nei][new_used]:
+                        dist[nei][new_used] = new_cost
+                        heapq.heappush(heap, (new_cost,nei,new_used))
+        return -1
+
+
+
+        

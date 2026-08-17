219d609334856479c2bbab9a198232ad4cbf11ab
phuongngu100 <135727156+phuongngu100@users.noreply.github.com>
2026-08-17 12:00:55 +0700

    Time: 58 ms (64.79%), Space: 25.9 MB (21.01%) - LeetHub

--- 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.py
+++ 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.py
@@ -1,14 +1,16 @@
 class Solution:
     def minimumCost(self, n: int, connections: List[List[int]]) -> int:
+        # this is a bidirectional; create a graph to map all the to and from
         graph = [[] for _ in range(n+1)]
-        for x, y, cost in connections:
+        for x,y,cost in connections:
             graph[x].append((y,cost))
             graph[y].append((x,cost))
-        
-        heap = [(0,1)] # cost, city
-        visited = set()
-        total = 0
 
+        # how to know that all the cities are connected? keep track of all the cities we have visited, if we reach the given city number, that means all are connected
+        visited = set()
+        # create a heap to keep track of the cheapest cost
+        heap = [(0, 1)] # cost, current city
+        total = 0 # we need to accumulate all the cost
         while heap:
             cost, city = heapq.heappop(heap)
             if city in visited:
@@ -17,9 +19,51 @@ class Solution:
             total += cost
             if len(visited) == n:
                 return total
+            
             for nei, price in graph[city]:
                 if nei not in visited:
-                    heapq.heappush(heap,(price, nei))
-        return -1
+                    heapq.heappush(heap, (price, nei))   
+        return -1             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # graph = [[] for _ in range(n+1)]
+        # for x, y, cost in connections:
+        #     graph[x].append((y,cost))
+        #     graph[y].append((x,cost))
+        
+        # heap = [(0,1)] # cost, city
+        # visited = set()
+        # total = 0
+
+        # while heap:
+        #     cost, city = heapq.heappop(heap)
+        #     if city in visited:
+        #         continue
+        #     visited.add(city)
+        #     total += cost
+        #     if len(visited) == n:
+        #         return total
+        #     for nei, price in graph[city]:
+        #         if nei not in visited:
+        #             heapq.heappush(heap,(price, nei))
+        # return -1
 
 

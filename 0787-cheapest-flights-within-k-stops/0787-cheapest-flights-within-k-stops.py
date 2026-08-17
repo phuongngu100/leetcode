2f17f4be7a137286b2a56c760ad3d7064b4b6697
phuongngu100 <135727156+phuongngu100@users.noreply.github.com>
2026-08-17 10:59:33 +0700

    Time: 8 ms (61.48%), Space: 21 MB (21.71%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,19 +1,33 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        # for this have create the original graph and the duplicate
         heap = [(0, src, 0)] # cost, src, number of stops
         graph = [[] for _ in range(n)]
         for fr, to, price in flights:
             graph[fr].append((to,price))
-        dist = [[float('inf')]*(k+2) for _ in range(n)] # + 2 for 
+        dist = [[float('inf')]*(k+2) for _ in range(n)] # + 2 for +1 as in 2 stops mean 3 flights (convert stops to flights) and another 1 since index start at 0, like the example below we need to get the 3
         dist[src][0] = 0 # example: dist[2][1] means we visit 2 cities with 1 flight
         '''
+        n = 4
+
+        flights = [
+            [0, 1, 100],
+            [0, 2, 500],
+            [1, 2, 100],
+            [2, 3, 100]
+        ]
+
+        src = 0
+        dst = 3
+        k = 2
+
         dist[city][number of flights] = cheapest price
         For every city, I'm keeping track of the cheapest price I've found for each possible number of flights.
                     NUMBER OF FLIGHTS
-                    0     1     2     3
+                      0     1     2     3
                 ┌─────┬─────┬─────┬─────┐
         city 0     │  0  │  ∞  │  ∞  │  ∞  │
-        city 1     │  ∞  │ 100 │  ∞  │  ∞  │
+        city 1     │  0  │ 100 │  ∞  │  ∞  │
         city 2     │  ∞  │ 500 │ 200 │  ∞  │
         city 3     │  ∞  │  ∞  │  ∞  │ 300 │
                 └─────┴─────┴─────┴─────┘
@@ -35,4 +49,7 @@ class Solution:
                         (new_cost, nei, new_stops)
                     )
         return -1
-        
+        # space is O(kE log(kN)) as k is number of stops, E is number of flights and n is number of cities
+        # we proceed each flights for number of stops, thus kE
+        # heap is log(kN), E can be up to n^2
+        # space is O(kN +E) for the dist and graph

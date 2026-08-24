064162ef12530c0ae92b06ea791791735c8768fc
phuongngu100 <135727156+phuongngu100@users.noreply.github.com>
2026-08-24 13:38:35 +0700

    Time: 13 ms (13.65%), Space: 19.7 MB (50.73%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,31 +1,55 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        n = len(edges)
-        graph = [[] for _ in range(n+1)]
+        parent = [i for i in range(len(edges) + 1)]
+        def find(x):
+            if parent[x] != x:
+                x = find(parent[x])
+            return parent[x]
+        def union(a,b):
+            root_a = find(a)
+            root_b = find(b)
 
-        # for a, b in edges:
-        #     edges[a].append(b)
-        #     edges[b].append(a)
+            # if find a cycle
+            if root_a == root_b:
+                return False
+            parent[root_a] = root_b
+            return True
         
-        def dfs(node, parent, target):
-            if node == target:
-                return True
-            visit.add(node)
-            for nei in graph[node]:
-                if nei == parent or nei in visit:
-                    continue
-                if dfs(nei, node, target):
-                    return True
-            return False
         for a,b in edges:
-            visit = set()
-            # If a and b are already connected,
-            # adding this edge creates a cycle
-            if dfs(a, -1, b):
-                return [a, b]
-            # Otherwise, safely add the edge
-            graph[a].append(b)
-            graph[b].append(a)
+            if not union(a,b):
+                return [a,b]
+
+
+
+
+
+
+
+
+
+
+        # n = len(edges)
+        # graph = [[] for _ in range(n+1)]
+        
+        # def dfs(node, parent, target):
+        #     if node == target:
+        #         return True
+        #     visit.add(node)
+        #     for nei in graph[node]:
+        #         if nei == parent or nei in visit:
+        #             continue
+        #         if dfs(nei, node, target):
+        #             return True
+        #     return False
+        # for a,b in edges:
+        #     visit = set()
+        #     # If a and b are already connected,
+        #     # adding this edge creates a cycle
+        #     if dfs(a, -1, b):
+        #         return [a, b]
+        #     # Otherwise, safely add the edge
+        #     graph[a].append(b)
+        #     graph[b].append(a)
 
 
         

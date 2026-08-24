a453a9e5a5e91f074f5452770d3a64f57dbd7c79
phuongngu100 <135727156+phuongngu100@users.noreply.github.com>
2026-08-24 12:38:34 +0700

    Time: 3 ms (74.52%), Space: 21.1 MB (24.63%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,7 +1,5 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        # if not prerequisites:
-        #     return [0]
         graph = [[] for _ in range(numCourses)]
         for course, pre in prerequisites:
             graph[pre].append(course)
@@ -9,24 +7,29 @@ class Solution:
         res = []
         
         def dfs(course):
+            '''
+            denote 0 as unvisited
+            1 as is visiting right now
+            2 as done with this course
+            after done put it to the result
+            '''        
             if state[course] == 1:
-                return False # detect cycle
+                return False # we detect a cycle
             if state[course] == 2:
-                return True # complete the process
-            # mark the current as visiting
-            state[course] = 1
+                return True # we done with this course
+            state[course] =1
             for nei in graph[course]:
                 if not dfs(nei):
                     return False
-            # finish this course
-            state[course] = 2
+            state[course] = 2 #done after dfs 
             res.append(course)
             return True
-        
         for course in range(numCourses):
             if not dfs(course):
-                return []
+                return [ ]
         return res[::-1]
+
+        
             
             
 

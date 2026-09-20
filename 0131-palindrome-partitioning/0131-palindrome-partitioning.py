e7295fb892cdf40e8fd3d23e4475a9165d5153db
phuongngu100 <135727156+phuongngu100@users.noreply.github.com>
2026-09-20 12:33:17 -0400

    Time: 43 ms (59.87%), Space: 34.1 MB (49.44%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -0,0 +1,26 @@
+class Solution:
+    def partition(self, s: str) -> list[list[str]]:
+        res = []
+
+        def backtrack(substring,i):
+            if i == len(s):
+                res.append(substring.copy())
+                return
+            for j in range(i, len(s)):
+                if self.isPali(s,i,j):
+                    substring.append(s[i:j+1])
+                    backtrack(substring, j+1)
+                    substring.pop()
+        backtrack([],0)
+        return res
+
+
+
+
+
+    def isPali(self, s,l,r):
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l, r = l+1, r-1
+        return True

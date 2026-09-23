2757219928d50fa0a0d0216682bacd9bfecf119c
phuongngu100 <135727156+phuongngu100@users.noreply.github.com>
2026-09-22 23:19:42 -0400

    Time: 7 ms (28.22%), Space: 19.2 MB (95.22%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -8,11 +8,13 @@ class Solution:
             for c in range(9):
                 if board[r][c] == '.':
                     continue
-                if (board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[r//3][c//3]):
+                if (board[r][c] in rows[r] or 
+                    board[r][c] in cols[c] or board[r][c] in squares[(r//3, c//3)]):
                     return False
-                col[c].add(board[c][c])
-                row[r].add(board[c][c])
-                squares
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r//3,c//3)].add(board[r][c])
+        return True
 
             
 
